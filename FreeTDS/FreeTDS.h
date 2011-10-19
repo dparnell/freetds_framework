1536d6f4ff05fdea75a648b8ea5fe4b9dc0525bb
Daniel Parnell <me@danielparnell.com>
2011-10-19 13:19:01 +1100

    Initial Commit

---
+++ FreeTDS/FreeTDS.h
@@ -0,0 +1,13 @@
+//
+//  FreeTDS.h
+//  FreeTDS
+//
+//  Created by Daniel Parnell on 19/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FreeTDS : NSObject
+
+@end
---
+++ FreeTDS/FreeTDS.m
@@ -0,0 +1,23 @@
+//
+//  FreeTDS.m
+//  FreeTDS
+//
+//  Created by Daniel Parnell on 19/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FreeTDS.h"
+
+@implementation FreeTDS
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
