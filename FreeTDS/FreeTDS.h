4f7e13efa79745545483908d965a05f530f8a08a
Daniel Parnell <me@danielparnell.com>
2011-10-20 21:50:36 +1100

    Starting to get ready to make a connection :)

--- FreeTDS/FreeTDS.h
+++ FreeTDS/FreeTDS.h
@@ -7,8 +7,40 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <FreeTDS/sybfront.h>
 #import <FreeTDS/sybdb.h>
 
-@interface FreeTDS : NSObject
+const extern NSString* FREETDS_SERVER;
+const extern NSString* FREETDS_USER;
+const extern NSString* FREETDS_PASS;
+const extern NSString* FREETDS_DATABASE;
+
+const extern NSString* FREETDS_OS_ERROR;
+const extern NSString* FREETDS_OS_CODE;
+const extern NSString* FREETDS_DB_ERROR;
+const extern NSString* FREETDS_DB_CODE;
+const extern NSString* FREETDS_SEVERITY;
+
+@class FreeTDS;
+
+@protocol FreeTDSDelegate <NSObject>
+
+- (int) handleMessage:(NSString*) message from:(FreeTDS*)free_tds;
+
+@end
+
+@interface FreeTDS : NSObject {
+@private
+    LOGINREC* login;
+    DBPROCESS* process;
+
+    NSException* to_throw;
+}
+
++ (FreeTDS*) connectionWithDictionary:(NSDictionary*)dictionary;
+- (id) initWithDictionary:(NSDictionary*)dictionary;
+
+@property (readonly) LOGINREC* login;
+@property (readonly) DBPROCESS* process;
 
 @end
--- FreeTDS/FreeTDS.m
+++ FreeTDS/FreeTDS.m
@@ -8,16 +8,117 @@
 
 #import "FreeTDS.h"
 
+const NSString* FREETDS_SERVER = @"Server";
+const NSString* FREETDS_USER = @"User";
+const NSString* FREETDS_PASS = @"Password";
+const NSString* FREETDS_DATABASE = @"Database";
+
+const NSString* FREETDS_OS_ERROR = @"OS Error Description";
+const NSString* FREETDS_OS_CODE = @"OS Error Code";
+const NSString* FREETDS_DB_ERROR = @"Database Error";
+const NSString* FREETDS_DB_CODE = @"Database Error Code";
+const NSString* FREETDS_SEVERITY = @"Severity";
+
+const NSLock* login_lock = nil;
+
 @implementation FreeTDS
 
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        // Initialization code here.
+@synthesize login, process;
+
+
+#pragma mark -
+#pragma mark Error handlers
+
+static NSException* login_exception = nil;
+
+static int err_handler(DBPROCESS *dbproc, int severity, int dberr, int oserr, char *dberrstr, char *oserrstr) {
+    NSException* ex = [NSException exceptionWithName: @"FreeTDS" 
+                                              reason: [NSString stringWithFormat: @"%d - %s", dberr, dberrstr] 
+                                            userInfo: [NSDictionary dictionaryWithObjectsAndKeys: 
+                                                       [NSString stringWithCString: oserrstr encoding: NSUTF8StringEncoding], FREETDS_OS_ERROR,
+                                                       [NSNumber numberWithInt: oserr], FREETDS_OS_CODE,
+                                                       [NSString stringWithCString: dberrstr encoding: NSUTF8StringEncoding], FREETDS_DB_ERROR,
+                                                       [NSNumber numberWithInt: dberr], FREETDS_DB_CODE,
+                                                       [NSNumber numberWithInt: severity], FREETDS_SEVERITY,
+                                                       nil]];
+    
+    FreeTDS* free_tds = (FreeTDS*)dbgetuserdata(dbproc);
+    if(free_tds) {
+        free_tds->to_throw = ex;
+    } else {
+        login_exception = ex;
     }
     
+    return INT_CONTINUE;
+}
+
+static int msg_handler(DBPROCESS *dbproc, DBINT msgno, int msgstate, int severity, char *msgtext, char *srvname, char *procname, int line) {
+    
+    return  0;
+}
+
+
+#pragma mark -
+#pragma mark Init and dealloc
+
++ (void) initialize {
+    login_lock = [[NSLock new] retain];
+}
+
++ (FreeTDS*) connectionWithDictionary:(NSDictionary*)dictionary {
+    return [[[FreeTDS alloc] initWithDictionary: dictionary] autorelease];
+}
+
+- (id) initWithDictionary:(NSDictionary*)dictionary {
+    self = [super init];
+    if(self) {
+        NSString* user = [dictionary objectForKey: FREETDS_USER];
+        NSString* password = [dictionary objectForKey: FREETDS_PASS];
+        NSString* server = [dictionary objectForKey: FREETDS_SERVER];
+        NSString* database = [dictionary objectForKey: FREETDS_DATABASE];
+        
+        if(dbinit() == FAIL) {
+            @throw [NSException exceptionWithName: @"FreeTDS" reason: NSLocalizedString(@"Could not initialize FreeTDS", @"dbinit failed") userInfo: nil];
+        }
+        
+        dberrhandle(err_handler);
+        dbmsghandle(msg_handler);
+        
+        login = dblogin();
+        if(user) {
+            dbsetluser(login, [user UTF8String]);
+        }
+        if(password) {
+            dbsetlpwd(login, [password UTF8String]);
+        }
+
+        [login_lock lock];
+        @try {
+            login_exception = nil;
+            process = dbopen(login, [server UTF8String]);
+            if(login_exception) {
+                @throw login_exception;
+            }
+        } @finally {
+            login_exception = nil;
+            [login_lock unlock];
+        }
+        
+        if(process) {
+            
+            
+            if(database) {
+                dbuse(process, [database UTF8String]);
+            }
+        } else {
+        }
+    }
     return self;
+}
+
+- (void)dealloc {
+    
+    [super dealloc];
 }
 
 @end
